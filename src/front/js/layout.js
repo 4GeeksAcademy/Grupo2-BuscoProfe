59172a4bd1c0d56b79f6350b5ff7e2bab3592cc3
Julieta <olivera.julieta1912@gmail.com>
2024-11-29 22:42:42 +0000

    agregamos vista login

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import Login from "./pages/login";
 
 //create your first component
 const Layout = () => {
@@ -24,6 +25,7 @@ const Layout = () => {
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
+                    <Login />
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
