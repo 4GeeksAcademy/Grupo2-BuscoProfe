b7fab7af35adf30d794dc0ff0f61c61d5ff39e5e
Jimena <jparatge@gmail.com>
2024-12-03 21:11:56 +0000

    select class arreglado

--- src/front/js/pages/selectclass.js
+++ src/front/js/pages/selectclass.js
@@ -5,13 +5,22 @@ import "../../styles/home.css";
 export const SelectClass = () => {
     const { store, actions } = useContext(Context);
 
-    return (
-        <div id="searchClassForm">
-            <h2 id="headerTitle">Seleccionar clase</h2>
-            
-        </div>
+    return <div className="container mt-5">
+            <h4 id="headerTitle">Resultados que coinciden con tu búsqueda</h4>           
 
-    )
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src="https://t4.ftcdn.net/jpg/00/85/77/75/360_F_85777561_m6EMdjM6Knkz7OLJmN5zr5ZeK359S3G5.jpg" className="card-img-top" alt="foto de juan perez" />
+                    <div className="card-body">
+
+                        <h5 className="card-title">Juan Pérez</h5>
+                        <p className="card-text">Calificación</p>
+                        <a href="#" className="btn btn-primary">Ver disponibilidad</a>
+                    </div>
+                </div>
+                
+            </div>
+        
+    
 }
 
 
