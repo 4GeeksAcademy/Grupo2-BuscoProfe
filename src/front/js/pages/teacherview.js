cba7a4602c4e3ca920f871bc23e7d7ed5f04c575
Jimena <jparatge@gmail.com>
2024-12-09 03:19:52 +0000

    arreglando el teacherview

--- src/front/js/pages/teacherview.js
+++ src/front/js/pages/teacherview.js
@@ -1,60 +1,71 @@
 import React from "react";
 import '../../styles/teacherView.css';
-import '../../styles/home.css';
+// import '../../styles/home.css';
 
 function TeacherView() {
     return (
         <div className="view-container">
-            {/* Header */}
-            <header className="view-header-teacher">
-                <span className="teacher-name">Juan Pérez</span>
-                <h4>Ing en sistemas</h4>
-                <img src="https://t4.ftcdn.net/jpg/00/85/77/75/360_F_85777561_m6EMdjM6Knkz7OLJmN5zr5ZeK359S3G5.jpg" className="card-img-top rounded-circle" alt="foto de juan perez" />
-                
-                <section className="calificaciones d-flex mx-3">
-                <span className="teacher-name">Calificación</span>
-                <div class="rating">
-                    <span className="star">★</span>
-                    <span className="star">★</span>
-                    <span className="star">★</span>
-                    <span className="star">★</span>
-                    <span className="star text-secondary">★</span>
-                </div>
-            </section>
-            </header>
+            <div className="header-teacher">
+                <header className="view-header-info">
+                    <section className="dashboard-stats">
+                        <div className="stat-box">
+                            <span className="teacher-name">Juan Pérez</span>
+                            <h4>Ing en sistemas</h4>
+                            <img src="https://t4.ftcdn.net/jpg/00/85/77/75/360_F_85777561_m6EMdjM6Knkz7OLJmN5zr5ZeK359S3G5.jpg" className="card-img-top rounded-circle" alt="foto de juan perez" />
 
-            {/* Stats */}
-            <section className="dashboard-stats">
-                <div className="stat-box">rating</div>
-                <div className="stat-box">clases agendadas para la semana</div>
-                <div className="stat-box">ver chats(?)</div>
-                <div className="stat-box calendar">
-                    <h3>calendario</h3>
+                            <section className="calificaciones d-flex mx-3">
+                                <span className="teacher-name">Calificación</span>
+                                <div class="rating m-2">
+                                    <span className="star">★</span>
+                                    <span className="star">★</span>
+                                    <span className="star">★</span>
+                                    <span className="star">★</span>
+                                    <span className="star text-secondary">★</span>
+                                </div>
+                            </section>
+                        </div>
+                        <div className="stat-box">
+                            <span className="teacher-name">Info</span>
+                            <p> Soy un apasionado de la ingeniería en sistemas, soy estudiante avanzado
+                                de la carrera y quisiera aportar con mis conocimientos a que más chicos y chicas
+                                se contagien y puedan avanzar en sus metas</p></div>
+                        <div className="stat-box">
+                            <span className="teacher-name">Materias</span>
+                            <p>
+                                <li>Matemática</li>
+                                <li>Física</li></p>
+                            <span className="teacher-name">Palabras Clave</span>
+                            <p>
+                                <li>Derivadas</li>
+                                <li>Funciones de tercer grado</li></p>
+                        </div>
+
+                    </section>
+                </header>
+            </div>
+            <div className= "container-footer">
+                <div className="calendar">
+                    <span className="teacher-name">Calendario</span>
                     <p>10:00am - matemática Joaquín</p>
                     <div className="calendar-preview">[Calendario Aquí]</div>
                 </div>
-            </section>
 
-            <header className="view-header">
-                <span className="teacher-name">Calificación</span>             
-            </header>
-            
+                <header className="view-header-coments">
+                    <span className="teacher-name">Comentarios</span>
 
-            <header className="view-header">
-                             
-            </header>
-
-            <section className="dashboard-stats">              
-                <div className="stat-box">Un genio, me re ayudó</div>
-                <div className="stat-box">Super guay</div>
-                <div className="stat-box">Capo total, lo recomiendo</div>
-                <div className="stat-box">En matemáticas, el mejor</div>
-                <div className="stat-box">Arreglamos un horario y cumplió sin problema. Responsable y divertido</div>
-                <div className="stat-box">Recomiendo 100%, lo volvería a contratar</div>
-            </section>
-
-        </div>
+                    <section className="dashboard-stats">
+                        <div className="stat-box">Un genio, me re ayudó</div>
+                        <div className="stat-box">Super guay</div>
+                        <div className="stat-box">Capo total, lo recomiendo</div>
+                        <div className="stat-box">En matemáticas, el mejor</div>
+                       
+                    </section>
+                    <button className="btn btn-primary">Agregar comentario</button>
+                </header>
+            </div>
+        </div >
     );
 }
 
 export default TeacherView;
+
